02179e59de576f4a34c6bebff35455ec9b126e2c
Sean Alden <sean.alden@great.sch.id>
2026-07-27 14:20:03 +0700

    update app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
@@ -29,6 +30,8 @@ class AppServiceProvider extends ServiceProvider
 
         // Panggil fungsi arsitektur pembatasan API di sini
         $this->configureRateLimiting();
+
+        Model::preventLazyLoading(app()->isProduction());
     }
 
        /**
