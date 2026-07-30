ce7d5efe5aa2ce8835462902dd3f72e1623fc75e
Sean Alden <sean.alden@great.sch.id>
2026-07-30 12:12:40 +0700

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Cart::class);
     }
+
+    public function messages()
+    {
+        return $this->hasMany(\App\Models\Message::class, 'sender_id');
+    }
 }
